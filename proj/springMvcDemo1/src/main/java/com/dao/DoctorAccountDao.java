c5d78984d53b387d38a170ffca2babe0cf20033d
liuyuhang791034063 <liuyuhang791034063@qq.com>
2020-03-10 20:57:41 +0800

    新增患者、医师账号dao文件

---
+++ proj/springMvcDemo1/src/main/java/com/dao/DoctorAccountDao.java
@@ -0,0 +1,8 @@
+package com.dao;
+
+public interface DoctorAccountDao {
+    /*
+    通过医师账号取id
+     */
+    int getIdByAccount(String account);
+}
---
+++ proj/springMvcDemo1/src/main/java/com/dao/PatientAccountDao.java
@@ -0,0 +1,8 @@
+package com.dao;
+
+public interface PatientAccountDao {
+    /*
+    通过患者账号取id
+     */
+    int getIdByAccount(String account);
+}
