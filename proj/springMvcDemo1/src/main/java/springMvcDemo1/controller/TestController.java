0b5e978aaa2b4d9ba82f3297b0d99bb9a10e172c
liuyuhang791034063 <liuyuhang791034063@qq.com>
2020-03-11 22:11:42 +0800

    修改配置bug

--- proj/springMvcDemo1/src/main/java/springMvcDemo1/controller/TestController.java
+++ proj/springMvcDemo1/src/main/java/springMvcDemo1/controller/TestController.java
@@ -1,16 +1,26 @@
 package springMvcDemo1.controller;
 
+import com.dao.PatientAccountDao;
+import com.entity.DBPatientAccount;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+
+@RequestMapping("/test")
 @Controller
 public class TestController {
-	@RequestMapping("/user/log")	//����
-	public String userlogin(String userAccount,String userPass,Model model) {
-			return "none";
+	@Autowired
+    private PatientAccountDao patientAccountDao;
+
+	@ResponseBody
+	@RequestMapping(value = "/json", method = RequestMethod.POST)
+	public int testJosn(@RequestBody DBPatientAccount patientAccount) {
+		int result = patientAccountDao.insertPatientAccount(patientAccount);
+		return 1;
 	}
-	
+
 }
 
 
