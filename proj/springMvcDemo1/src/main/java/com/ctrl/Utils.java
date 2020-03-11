7f327e0cbfff49916aab73769a6e9b374e55408b
baotong.wbt <1176611589@qq.com>
2020-03-11 10:04:35 +0800

    utils.java  add getDateByStr

--- proj/springMvcDemo1/src/main/java/com/ctrl/Utils.java
+++ proj/springMvcDemo1/src/main/java/com/ctrl/Utils.java
@@ -1,5 +1,12 @@
 package com.ctrl;
 
-public class Utils {
+import java.util.Date;
 
+public class Utils {
+	static public Date getDateByStr(String yearString,String monthString,String dayString) {
+		int year = Integer.valueOf(yearString);
+		int month = Integer.valueOf(monthString);
+		int day = Integer.valueOf(dayString);
+		return new Date(year-1900,month-1,day);
+	}
 }
